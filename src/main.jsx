5e5bf10269f66b67caa99e0a9923edeebcf5a348
fribery <fribery.blog@gmail.com>
2025-12-21 23:28:43 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { init, miniApp, mainButton, shareURL } from '@tma/sdk';
+import { init, miniApp, mainButton, shareURL } from '@telegram-apps/sdk';
 
 const initializeTelegramSDK = async () => {
   try {
@@ -51,7 +51,7 @@ const initializeTelegramSDK = async () => {
       });
     }
 
-
+    
 initializeTelegramSDK();
 
 
